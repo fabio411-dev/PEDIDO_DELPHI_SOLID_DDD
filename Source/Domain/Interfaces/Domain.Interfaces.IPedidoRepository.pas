55047a720f4f05178749ad2017f3af7149db00d7
fabioakiyama <fabio.lara@multidadosti.com.br>
2026-07-10 11:15:32 -0300

    Alteração : Permitir pesquisa com parcial do nome do cliente

--- Source/Domain/Interfaces/Domain.Interfaces.IPedidoRepository.pas
+++ Source/Domain/Interfaces/Domain.Interfaces.IPedidoRepository.pas
@@ -12,7 +12,7 @@ type
     ['{AC9B7EF2-3A84-4C5D-BE56-78F3FA6F8E2A}']
 
     function  FindById(AIdPedido: Integer): TPedido;
-    function  FindAll: TObjectList<TPedido>;
+    function  FindAll(APartialDesc : String): TObjectList<TPedido>;
     procedure Add(const APedido: TPedido);
     procedure Update(const APedido: TPedido);
     procedure Delete(AIdPedido: Integer);
